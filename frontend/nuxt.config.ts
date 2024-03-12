f9eb11b78b86bdb98b4167477b67eb10c5c1b980
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-12 06:28:00 +0530

    install primevue
    setup primevue and menubar was used

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css','primevue/resources/themes/aura-light-green/theme.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  modules: ['@nuxtjs/color-mode'],
+  modules: ['@nuxtjs/color-mode','nuxt-primevue'],
   colorMode: {
       classSuffix: '',
       preference: 'light',
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,17 +2,8 @@
   <div class="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-200">
     <nav class="bg-white dark:bg-gray-800">
       <div class="container mx-auto px-6 py-3 flex justify-between items-center">
-        <a href="#" class="text-gray-900 dark:text-white text-3xl font-bold">Blog</a>
-        <div class="flex space-x-4">
-          <a href="#" class="text-gray-900 dark:hover:text-blue-400 hover:text-blue-600 transition duration-300">Home</a>
-          <a href="#" class="text-gray-900 dark:hover:text-blue-400 hover:text-blue-600 transition duration-300">About</a>
-          <a href="#" class="text-gray-900 dark:hover:text-blue-400 hover:text-blue-600 transition duration-300">Contact</a>
-        </div>
-        <div class="space-x-4">
-          <ButtonColorModeSwitcher/>
-          <button class="px-4 py-2 bg-blue-500 rounded hover:bg-blue-600 transition duration-300">Sign In</button>
-          <button class="px-4 py-2 bg-blue-500 rounded hover:bg-blue-600 transition duration-300">Register</button>
-        </div>
+          <FullPageMenuBar/>
+          <!-- <ButtonColorModeSwitcher/> -->
       </div>
     </nav>
     <header class="bg-white dark:bg-gray-800 text-gray-900 dark:text-white py-8">
