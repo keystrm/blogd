a2776def6e0a3a062814b63494388aba4d31fe8e
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-27 00:42:15 +0530

    turn off the watcher

--- frontend/composables/useApiFetch.ts
+++ frontend/composables/useApiFetch.ts
@@ -22,6 +22,7 @@ export function useApiFetch<T>(path: string | (() => string), options: UseFetchO
     baseURL: config.public.apiBaseUrl,
     headers,
     credentials: "include",
+    watch:false,
     ...options,
   });
 }
